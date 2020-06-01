302059bf2bbbae9b389043befd5a0661a83cd6e6
Xenoret <63548161+Xenoret@users.noreply.github.com>
2020-06-01 15:42:17 +0300

    Update Golden_coin.py

--- Golden_coin.py
+++ Golden_coin.py
@@ -5,7 +5,8 @@
 # Golden coin / try your chois @by Xenoret #
 ############################################
 
-import os, random #import libs
+import os #import lib
+import random #import lib
 
 os.system('cls||clear') # Clear terminal
 
@@ -16,10 +17,13 @@ print('=====' *10)
 def coin():
     print('This golden coin, will decide how you do\n')
     rand = random.randint(1, 3) # Random try value
+    
     if rand == 1:
         print('Eagle!\nGo to sleep.')
+    
     elif rand == 2:
         print('Tails!\nGo back to code.')
+    
     else:
         print('Rib coin!\nFree time.')
 coin() #Call def
